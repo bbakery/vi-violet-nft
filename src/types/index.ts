d3adef4f56748318b20a1e1df66a84e358a28e51
bbakery <bbakery@proton.me>
2025-07-20 07:08:30 +0300

    Виправлено конфлікт типів window.ethereum

--- src/types/index.ts
+++ src/types/index.ts
@@ -49,14 +49,3 @@ export interface WalletInfo {
     balance: { [tokenAddress: string]: number };
     isConnected: boolean;
 }
-
-// Ethereum types
-declare global {
-    interface Window {
-        ethereum?: {
-            request: (args: { method: string; params?: any[] }) => Promise<any>;
-            on: (event: string, callback: (params: any) => void) => void;
-            removeListener: (event: string, callback: (params: any) => void) => void;
-        };
-    }
-}
